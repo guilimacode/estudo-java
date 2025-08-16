88213b4ade13d67cc4d63fe80c9aa2c22b625cf6
Guilherme Sousa <antonioguilhermeslima@gmail.com>
2025-08-16 12:07:44 -0300

    Estruturas condicionais

---
+++ src/antonioguilhermedev/estudojava/introducao/EstruturasCondicionaisExercicio.java
@@ -0,0 +1,32 @@
+package antonioguilhermedev.estudojava.introducao;
+
+import java.sql.SQLOutput;
+
+public class EstruturasCondicionaisExercicio {
+    public static void main(String[] args) {
+        int idade = 23;
+
+        if (idade < 15) {
+            System.out.println("Você pertence a categoria infantil!");
+        } else if (idade >= 15 && idade <= 18) {
+            System.out.println("Você pertence a categoria juvenil!");
+        } else {
+            System.out.println("Você pertence a categoria adulta!");
+        }
+
+        double salarioAnual = 44000;
+        double primeiraFaixa = 9.70 / 100;
+        double segundaFaixa = 37.35 / 100;
+        double terceiraFaixa = 49.50 / 100;
+        double valorASerPago;
+
+        if (salarioAnual >= 0 && salarioAnual <= 34.712) {
+            valorASerPago = salarioAnual * primeiraFaixa;
+        } else if (salarioAnual >= 34.713 && salarioAnual <= 68.507) {
+            valorASerPago = salarioAnual * segundaFaixa;
+        }  else {
+            valorASerPago = salarioAnual * terceiraFaixa;
+        }
+        System.out.println("Valor a ser pago em imposto: " + valorASerPago);
+    }
+}
