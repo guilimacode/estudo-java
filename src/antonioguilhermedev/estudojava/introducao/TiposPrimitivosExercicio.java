70c3011efa3a6b71547454d9553744de3a7d6175
Guilherme Sousa <antonioguilhermeslima@gmail.com>
2025-08-16 12:06:14 -0300

    Tipos Primitivos

---
+++ src/antonioguilhermedev/estudojava/introducao/TiposPrimitivosExercicio.java
@@ -0,0 +1,14 @@
+package antonioguilhermedev.estudojava.introducao;
+
+public class TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "Guilherme";
+        String endereco = "Rua A, Bairro B, Cidade C";
+        double salario = 2700;
+        String dataRecebimentoSalario = "10/01/2025";
+
+        System.out.println("Eu, " + nome + " morando no endereço " + endereco + ", confirmo que recebi o salário de " + salario + ", na data " + dataRecebimentoSalario);
+
+
+    }
+}
