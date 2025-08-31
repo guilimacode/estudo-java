9c814a685f9d8ed3a52883c5bb1557b8f48e73ec
Guilherme Sousa <antonioguilhermeslima@gmail.com>
2025-08-31 11:25:55 -0300

    modificador final - tipo referencia

--- src/guilimacode/estudojava/javacore/modificadorfinal/domain/Carro.java
+++ src/guilimacode/estudojava/javacore/modificadorfinal/domain/Carro.java
@@ -3,6 +3,7 @@ package guilimacode.estudojava.javacore.modificadorfinal.domain;
 public class Carro {
     private String marca;
     public static final double VELOCIDADE_LIMITE = 250;
+    public final Comprador COMPRADOR = new Comprador();
 
     public String getMarca() {
         return marca;
--- src/guilimacode/estudojava/javacore/modificadorfinal/test/CarroTest.java
+++ src/guilimacode/estudojava/javacore/modificadorfinal/test/CarroTest.java
@@ -1,11 +1,16 @@
 package guilimacode.estudojava.javacore.modificadorfinal.test;
 
 import guilimacode.estudojava.javacore.modificadorfinal.domain.Carro;
+import guilimacode.estudojava.javacore.modificadorfinal.domain.Comprador;
 
 public class CarroTest {
     public static void main(String[] args) {
         Carro carro = new Carro();
         carro.setMarca("Ford");
+        Comprador comprador2 = new Comprador();
         System.out.println(Carro.VELOCIDADE_LIMITE);
+        System.out.println(carro.COMPRADOR);
+        carro.COMPRADOR.setNome("Fernando");
+        System.out.println(carro.COMPRADOR);
     }
 }
